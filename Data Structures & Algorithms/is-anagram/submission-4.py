57f094dd955273eda83bc8410df89ee9ba5500b9
Kfir Sadeh <kfirsad@gmail.com>
2026-07-06 21:41:00 +0300

    Add: is-anagram - submission-4

---
+++ Data Structures & Algorithms/is-anagram/submission-4.py
@@ -0,0 +1,22 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        if len(s) != len(t):
+            return False
+
+        myMap = {}
+
+        for s in s:
+            if s in myMap:
+                myMap[s] += 1
+            else:
+                myMap[s] = 1
+        
+        for s in t:
+            if s in myMap:
+                myMap[s] -= 1
+                if(myMap[s] < 0):
+                    return False
+            else:
+                return False
+
+        return True
