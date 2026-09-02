cce69d98c44a3e11e325ed8dd0d3738bad38bb3c
Kfir Sadeh <kfirsad@gmail.com>
2026-09-03 00:26:29 +0300

    Add: buy-and-sell-crypto - submission-3

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-3.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int maxProfit(int[] prices) {
+        int left = 0;
+        int right = 1;
+        int maxProfit = 0;
+
+        while(right < prices.length){
+            if(prices[left] < prices[right]){
+                int currProfit = prices[right] - prices[left];
+                if(currProfit > maxProfit){
+                    maxProfit = currProfit;
+                }
+            }else{
+                left = right;
+            }
+
+            right++;
+
+        }
+
+        return maxProfit;
+    }
+}
