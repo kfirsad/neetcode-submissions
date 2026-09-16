e435302ac8e5111a9d3a5a25bfed1fd294f05b3d
Kfir Sadeh <kfirsad@gmail.com>
2026-09-16 20:23:41 +0300

    Add: house-robber-ii - submission-5

---
+++ Data Structures & Algorithms/house-robber-ii/submission-5.java
@@ -0,0 +1,28 @@
+class Solution {
+    public int rob(int[] nums) {
+        
+        if(nums.length == 1){
+            return nums[0];
+        }else if(nums.length == 2){
+            return Math.max(nums[0], nums[1]);
+        }
+
+        int maxRouteA = checkMaxPrice(nums, 0, nums.length - 1);
+        int maxRouteB = checkMaxPrice(nums, 1, nums.length);
+        return Math.max(maxRouteA, maxRouteB);
+    }
+
+    public int checkMaxPrice(int[] nums, int startIndex, int endIndex){
+        int prevPrevHouse = nums[startIndex];
+        int prevHouse = Math.max(prevPrevHouse, nums[startIndex + 1]);
+        int maxProfit = prevHouse;
+
+        for(int i = startIndex + 2; i < endIndex; i++){
+            maxProfit = Math.max(prevHouse, prevPrevHouse + nums[i]);
+            prevPrevHouse = prevHouse;
+            prevHouse = maxProfit;
+        }
+
+        return maxProfit;
+    }
+}
