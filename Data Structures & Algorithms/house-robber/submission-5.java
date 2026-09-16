713100af260622098f534cf350407f4db5da4007
Kfir Sadeh <kfirsad@gmail.com>
2026-09-16 20:02:43 +0300

    Add: house-robber - submission-5

---
+++ Data Structures & Algorithms/house-robber/submission-5.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int rob(int[] nums) {
+
+        if(nums.length == 1){
+            return nums[0];
+        }
+
+        int prevPrevHouse = nums[0];
+        int prevHouse = Math.max(prevPrevHouse, nums[1]);
+        int maxProfit = prevHouse;
+
+        for(int i = 2; i < nums.length; i++){
+            maxProfit = Math.max(prevHouse, prevPrevHouse + nums[i]);
+            prevPrevHouse = prevHouse;
+            prevHouse = maxProfit;
+        }
+
+        return maxProfit;
+
+    }
+}
