697d13ca0db67871b93d4949c26e307abb9d2749
Kfir Sadeh <kfirsad@gmail.com>
2026-09-26 19:36:48 +0300

    Add: search-for-word - submission-1

---
+++ Data Structures & Algorithms/search-for-word/submission-1.java
@@ -0,0 +1,48 @@
+class Solution {
+    private int[][] directions = new int[][]{{1,0}, {0,1}, {-1,0}, {0,-1}};
+
+    public boolean exist(char[][] board, String word) {
+        int rows = board.length;
+        int cols = board[0].length;
+
+        for(int i = 0; i < rows; i++){
+            for(int j = 0; j < cols; j++){
+                if(board[i][j] == word.charAt(0)){
+                    if(callDFS(board, i, j, rows, cols, word, 0)){
+                        return true;
+                    }
+                }
+            }
+        }
+
+        return false;
+    }
+
+    public boolean callDFS(char[][] board, int i, int j, int rows, int cols, String word, int letterIndex){
+        if(i < 0 || i >= rows || j < 0 || j >= cols || board[i][j] != word.charAt(letterIndex)){
+            return false;
+        }
+
+        if(letterIndex == word.length() - 1){
+            return true;
+        }
+
+        char temp = board[i][j];
+        board[i][j] = '@';
+
+
+        for(int[] dir : directions){
+            int newRow = i + dir[0];
+            int newCol = j + dir[1];
+
+
+            if(callDFS(board, newRow, newCol, rows, cols, word, letterIndex + 1)){
+                board[i][j] = temp;
+                return true;
+            }
+        }
+
+        board[i][j] = temp;
+        return false;
+    }
+}
